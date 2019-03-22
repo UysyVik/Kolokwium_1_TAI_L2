5fb728d01df9c6f142a0d3159ae9ebd38e8d7c09
miłosz lazarowicz <miloszlazar@gmail.com>
2019-03-22 16:39:40 +0100

    Milosz Lazarowicz

---
+++ navbar.js
@@ -0,0 +1,8 @@
+let nav = '<h1>Menu</h1>'+
+	'<a href="index.html">Home </a>'+
+	'<a href="blog.html">Blog</a>';
+   
+let elem = document.querySelector('.nav-menu');
+elem.innerHTML = '<div>'+nav+'</div>';
+
+
